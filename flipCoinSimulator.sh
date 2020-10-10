3dd63e63163a0fd8b7539ea513244a71ade9da68
unknown <ravibs1994@gmail.com>
2020-10-10 20:50:51 +0530

    IfTieThenRunUntilDefIsTwo

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,7 +5,7 @@ hCount=0
 tCount=0
 while [[ $hCount -lt 21 && $tCount -lt 21 ]]
 	do
-		result=$(($RANDOM%2))
+			result=$(($RANDOM%2))
 			if [ $result -eq $head ]
 				then
 					((hCount++))
@@ -13,9 +13,25 @@ while [[ $hCount -lt 21 && $tCount -lt 21 ]]
 					((tCount++))
 			fi
 done
+echo "HCount "$hCount
+echo "TCount "$tCount
 if [ $hCount -eq $tCount ]
 	then
-			echo "Tie"
+		echo "ITS TAI"
+		echo "Def" $(($hCount-$tCount))
+		while [ $(($hCount-$tCount )) -ne 2 ] && [ $(($tCount-$hCount )) -ne 2 ] 
+			do
+			rand=$((RANDOM%2))
+        		case $rand in
+                0)
+                ((hCount++));;
+                1)
+                ((tCount++));;
+                *);;
+        esac
+	done
+	 echo "Head Wins "$hCount
+ 	echo "Tail Wins "$tCount
 	elif [ $hCount -gt $tCount ]
 		then
 			echo "Head Wins "$hCount
